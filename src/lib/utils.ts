8a4743ca86b7bc0826759afa371c85d3faafafbe
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2025-05-14 01:02:01 +0700

    feat: create function get status text Order

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { OrderStatus } from "@prisma/client";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getStatusText = (status: OrderStatus): string => {
+  switch (status) {
+    case "Pending":
+      return "รอชำระเงิน";
+    case "Paid":
+      return "ชำระเงินแล้ว";
+    case "Shipped":
+      return "จัดส่งแล้ว";
+    case "Delivered":
+      return "ได้รับสินค้าแล้ว";
+    case "Cancelled":
+      return "ยกเลิกแล้ว";
+    default:
+      return status;
+  }
+};
